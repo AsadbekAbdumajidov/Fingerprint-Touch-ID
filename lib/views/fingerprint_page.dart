0511fdb6b0f5fe76139a9615398bbfcb7a7b0bfe
Asadbek <asadbekabdumajidov7@gmail.com>
2022-07-05 12:49:34 +0500

    required files have been entered

---
+++ lib/views/fingerprint_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FingerPrintPAge extends StatelessWidget {
+  const FingerPrintPAge({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
