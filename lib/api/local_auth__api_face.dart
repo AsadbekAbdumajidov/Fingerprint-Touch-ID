9984a954af2ebde475ba2172fd0cfc9d8eee89ca
Asadbek <asadbekabdumajidov7@gmail.com>
2022-07-06 11:23:04 +0500

    Added fingerprint scanner

---
+++ lib/api/local_auth__api_face.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/services.dart';
+// import 'package:local_auth/local_auth.dart';
+
+// class LocalAuthApi {
+//   static final _auth = LocalAuthentication();
+
+//   static Future<bool> hasBiometrics() async {
+//     try {
+//       return await _auth.canCheckBiometrics;
+//     } on PlatformException {
+//       return false;
+//     }
+//   }
+
+//   static Future<bool> authenticate() async {
+//     final isAvailable = await hasBiometrics();
+//     if (!isAvailable) return false;
+
+//     try {
+//       return await _auth.authenticate(
+//           localizedReason: 'Scan Face to Authenticate',
+//           options: const AuthenticationOptions(
+//               biometricOnly: true, stickyAuth: false, useErrorDialogs: false));
+//     } on PlatformException {
+//       return false;
+//     }
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:fingerprint/views/fingerprint_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const String title = 'Local Auth';
   const MyApp({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Fingerprint & Touch ID',
-      home: FingerPrintPAge(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: const FingerPage(),
+      );
 }
